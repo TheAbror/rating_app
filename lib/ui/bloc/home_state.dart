1f6ea322b0ceb06d594a8a51292033dee2fd2dba
Elbarto <oyjddkvj@gmail.com>
2023-12-23 20:48:39 +0500

    added isDark

--- lib/ui/bloc/home_state.dart
+++ lib/ui/bloc/home_state.dart
@@ -1,26 +1,31 @@
 part of 'home_bloc.dart';
 
 class HomeState extends Equatable {
+  final bool isDark;
   final BlocProgress blocProgress;
   final String failureMessage;
 
   const HomeState({
+    required this.isDark,
     required this.blocProgress,
     required this.failureMessage,
   });
 
   factory HomeState.initial() {
     return HomeState(
+      isDark: false,
       blocProgress: BlocProgress.IS_SUCCESS,
       failureMessage: '',
     );
   }
 
   HomeState copyWith({
+    bool? isDark,
     BlocProgress? blocProgress,
     String? failureMessage,
   }) {
     return HomeState(
+      isDark: isDark ?? this.isDark,
       blocProgress: blocProgress ?? this.blocProgress,
       failureMessage: failureMessage ?? this.failureMessage,
     );
@@ -28,6 +33,7 @@ class HomeState extends Equatable {
 
   @override
   List<Object?> get props => [
+        isDark,
         blocProgress,
         failureMessage,
       ];
