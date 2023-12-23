a4de49270cd2d5e413eb889cca67b694ced3c4a7
Elbarto <oyjddkvj@gmail.com>
2023-12-23 20:23:24 +0500

    column to Listview

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -13,26 +13,28 @@ class HomePage extends StatelessWidget {
         width: double.infinity,
         padding: EdgeInsets.symmetric(vertical: 50.h),
         decoration: HomePageBackgroundColor(),
-        child: Column(
+        child: ListView(
           children: [
             SizedBox(height: 50.h),
-            Wrap(
-              spacing: 27.w,
-              runSpacing: 15.h,
-              children: const [
-                HomePageBodyItem(text: 'Schools', icon: 'assets/icons/school.png'),
-                HomePageBodyItem(text: 'Restaurants', icon: 'assets/icons/restaurant.png'),
-                HomePageBodyItem(text: 'Parks', icon: 'assets/icons/park.png'),
-                HomePageBodyItem(text: 'Cars', icon: 'assets/icons/car-chevrolet.png'),
-                HomePageBodyItem(text: 'Fast Food', icon: 'assets/icons/fastfood.png'),
-                HomePageBodyItem(text: 'Hotels', icon: 'assets/icons/hotel.png'),
-                HomePageBodyItem(text: 'Kindergartens', icon: 'assets/icons/kindergarten.png'),
-                HomePageBodyItem(text: 'Social Media', icon: 'assets/icons/SocialMedia.png'),
-                HomePageBodyItem(text: 'Software Engineers', icon: 'assets/icons/softwareE.png'),
-                HomePageBodyItem(text: 'Software Engineers', icon: 'assets/icons/softwareE.png'),
-                HomePageBodyItem(text: 'Universities', icon: 'assets/icons/student.png'),
-                HomePageBodyItem(text: 'Video Platforms', icon: 'assets/icons/videoPlatform.png'),
-              ],
+            Center(
+              child: Wrap(
+                spacing: 27.w,
+                runSpacing: 15.h,
+                children: const [
+                  HomePageBodyItem(text: 'Schools', icon: 'assets/icons/school.png'),
+                  HomePageBodyItem(text: 'Restaurants', icon: 'assets/icons/restaurant.png'),
+                  HomePageBodyItem(text: 'Parks', icon: 'assets/icons/park.png'),
+                  HomePageBodyItem(text: 'Cars', icon: 'assets/icons/car-chevrolet.png'),
+                  HomePageBodyItem(text: 'Fast Food', icon: 'assets/icons/fastfood.png'),
+                  HomePageBodyItem(text: 'Hotels', icon: 'assets/icons/hotel.png'),
+                  HomePageBodyItem(text: 'Kindergartens', icon: 'assets/icons/kindergarten.png'),
+                  HomePageBodyItem(text: 'Social Media', icon: 'assets/icons/SocialMedia.png'),
+                  HomePageBodyItem(text: 'Software Engineers', icon: 'assets/icons/softwareE.png'),
+                  HomePageBodyItem(text: 'Software Engineers', icon: 'assets/icons/softwareE.png'),
+                  HomePageBodyItem(text: 'Universities', icon: 'assets/icons/student.png'),
+                  HomePageBodyItem(text: 'Video Platforms', icon: 'assets/icons/videoPlatform.png'),
+                ],
+              ),
             ),
           ],
         ),
