f9a751938c90a5dc793b38f52fcccb7d18f46e3e
Elbarto <oyjddkvj@gmail.com>
2023-12-23 10:41:59 +0500

    added custom feautures

---
+++ lib/core/api/api_provider.dart
@@ -0,0 +1,46 @@
+// import 'dart:io';
+// import 'package:chopper/chopper.dart';
+// import 'package:http/io_client.dart' as http;
+// import 'package:rating_app/core/api/custom_converter.dart';
+
+// class ApiProvider {
+//   static late ChopperClient _client;
+//   static late UserService userService;
+
+//   ///Services
+//   static create({String? token}) {
+//     _client = ChopperClient(
+//       client: http.IOClient(
+//         HttpClient()..connectionTimeout = const Duration(seconds: 40),
+//       ),
+//       services: [
+//         UserService.create(),
+//       ],
+//       interceptors: getInterceptors(token),
+//       converter: CustomDataConverter(),
+//     );
+
+//     userService = _client.getService<UserService>();
+//   }
+
+//   static List getInterceptors(token) {
+//     List interceptors = [];
+
+//     interceptors.add(HttpLoggingInterceptor());
+
+//     interceptors.add(HeadersInterceptor({
+//       HttpHeaders.acceptHeader: 'application/json',
+//       HttpHeaders.contentTypeHeader: 'application/json',
+//       HttpHeaders.authorizationHeader: token != null ? 'Bearer $token' : '',
+//     }));
+
+//     return interceptors;
+//   }
+
+//   static dispose() {
+//     _client.dispose();
+//   }
+// }
+
+// class UserService {
+// }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:rating_app/core/colors/app_colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: ListView(children: [
+        Container(
+          margin: EdgeInsets.only(left: 16.w),
+          padding: EdgeInsets.all(8.w),
+          decoration: BoxDecoration(
+            color: AppColors.background,
+            borderRadius: BorderRadius.circular(8.r),
+            border: Border.all(
+              color: AppColors.outline,
+              width: 0.5.w,
+            ),
+          ),
+        ),
+      ]),
+    );
   }
 }
